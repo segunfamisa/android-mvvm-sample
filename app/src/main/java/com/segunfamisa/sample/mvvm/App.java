f44e2e6869a0cf6fe8e506389749a9d52da6d47f
Segun Famisa <segunfamisa@gmail.com>
2017-03-08 13:32:56 +0100

    chore: Added Application class

---
+++ app/src/main/java/com/segunfamisa/sample/mvvm/App.java
@@ -0,0 +1,12 @@
+package com.segunfamisa.sample.mvvm;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
