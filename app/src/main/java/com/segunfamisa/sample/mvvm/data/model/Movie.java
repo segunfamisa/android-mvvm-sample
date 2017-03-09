e3461bec9f1052ced550adf995c4119f44a37283
Segun Famisa <segunfamisa@gmail.com>
2017-03-09 07:07:08 +0100

    chore: Added builder class to Movie data class

--- app/src/main/java/com/segunfamisa/sample/mvvm/data/model/Movie.java
+++ app/src/main/java/com/segunfamisa/sample/mvvm/data/model/Movie.java
@@ -135,6 +135,141 @@ public class Movie {
     @Override
     public String toString() {
         return getTitle();
+    }
 
+    private Movie(String posterPath, boolean adult, String overview, String releaseDate, long id, String title, String backdropPath, double popularity, long voteCount, boolean video, double voteAverage, String imdbId, String homepageUrl, long runtime, long budget, List<ProductionCompany> productionCompanies, String tagline, String status) {
+        this.posterPath = posterPath;
+        this.adult = adult;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.id = id;
+        this.title = title;
+        this.backdropPath = backdropPath;
+        this.popularity = popularity;
+        this.voteCount = voteCount;
+        this.video = video;
+        this.voteAverage = voteAverage;
+        this.imdbId = imdbId;
+        this.homepageUrl = homepageUrl;
+        this.runtime = runtime;
+        this.budget = budget;
+        this.productionCompanies = productionCompanies;
+        this.tagline = tagline;
+        this.status = status;
+    }
+
+    public static class Builder {
+        private String posterPath;
+        private boolean adult;
+        private String overview;
+        private String releaseDate;
+        private long id;
+        private String title;
+        private String backdropPath;
+        private double popularity;
+        private long voteCount;
+        private boolean video;
+        private double voteAverage;
+        private String imdbId;
+        private String homepageUrl;
+        private long runtime;
+        private long budget;
+        private List<ProductionCompany> productionCompanies;
+        private String tagline;
+        private String status;
+
+        public Builder setPosterPath(String posterPath) {
+            this.posterPath = posterPath;
+            return this;
+        }
+
+        public Builder setAdult(boolean adult) {
+            this.adult = adult;
+            return this;
+        }
+
+        public Builder setOverview(String overview) {
+            this.overview = overview;
+            return this;
+        }
+
+        public Builder setReleaseDate(String releaseDate) {
+            this.releaseDate = releaseDate;
+            return this;
+        }
+
+        public Builder setId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setBackdropPath(String backdropPath) {
+            this.backdropPath = backdropPath;
+            return this;
+        }
+
+        public Builder setPopularity(double popularity) {
+            this.popularity = popularity;
+            return this;
+        }
+
+        public Builder setVoteCount(long voteCount) {
+            this.voteCount = voteCount;
+            return this;
+        }
+
+        public Builder setVideo(boolean video) {
+            this.video = video;
+            return this;
+        }
+
+        public Builder setVoteAverage(double voteAverage) {
+            this.voteAverage = voteAverage;
+            return this;
+        }
+
+        public Builder setImdbId(String imdbId) {
+            this.imdbId = imdbId;
+            return this;
+        }
+
+        public Builder setHomepageUrl(String homepageUrl) {
+            this.homepageUrl = homepageUrl;
+            return this;
+        }
+
+        public Builder setRuntime(long runtime) {
+            this.runtime = runtime;
+            return this;
+        }
+
+        public Builder setBudget(long budget) {
+            this.budget = budget;
+            return this;
+        }
+
+        public Builder setProductionCompanies(List<ProductionCompany> productionCompanies) {
+            this.productionCompanies = productionCompanies;
+            return this;
+        }
+
+        public Builder setTagline(String tagline) {
+            this.tagline = tagline;
+            return this;
+        }
+
+        public Builder setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Movie build() {
+            return new Movie(posterPath, adult, overview, releaseDate, id, title, backdropPath, popularity, voteCount, video, voteAverage, imdbId, homepageUrl, runtime, budget, productionCompanies, tagline, status);
+        }
     }
 }
