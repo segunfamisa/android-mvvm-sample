9811a2cf7ea05ab5179b9cf79d5e0679206eb136
Segun Famisa <segunfamisa@gmail.com>
2017-03-09 07:16:11 +0100

    chore: Launched Movies fragment when MainActivity starts

--- app/src/main/java/com/segunfamisa/sample/mvvm/ui/MainActivity.java
+++ app/src/main/java/com/segunfamisa/sample/mvvm/ui/MainActivity.java
@@ -1,15 +1,26 @@
 package com.segunfamisa.sample.mvvm.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.segunfamisa.sample.mvvm.R;
+import com.segunfamisa.sample.mvvm.ui.movies.MoviesFragment;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            Fragment fragment = MoviesFragment.newInstance();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.container, fragment)
+                    .commit();
+        }
     }
 }
