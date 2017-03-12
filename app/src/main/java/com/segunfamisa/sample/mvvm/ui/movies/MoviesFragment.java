0098fd8a791e8f15eb8afcbafd9d71d63be8346d
Segun Famisa <segunfamisa@gmail.com>
2017-03-12 12:26:06 +0100

    chore: Updated the app theme and MoviesFragment layout

--- app/src/main/java/com/segunfamisa/sample/mvvm/ui/movies/MoviesFragment.java
+++ app/src/main/java/com/segunfamisa/sample/mvvm/ui/movies/MoviesFragment.java
@@ -5,6 +5,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.GridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,7 @@ import com.segunfamisa.sample.mvvm.data.remote.api.MovieApiService;
 import com.segunfamisa.sample.mvvm.data.repository.MoviesRemoteRepository;
 import com.segunfamisa.sample.mvvm.databinding.MovieListBinding;
 import com.segunfamisa.sample.mvvm.ui.base.BaseFragment;
+import com.segunfamisa.sample.mvvm.ui.moviedetails.MovieDetailsFragment;
 
 /**
  * A fragment to list the movies
@@ -47,7 +49,7 @@ public class MoviesFragment extends BaseFragment implements Interactor {
         super.onViewCreated(view, savedInstanceState);
 
         setupViewModels();
-
+        setupToolbar();
         setupRecyclerView();
     }
 
@@ -56,6 +58,10 @@ public class MoviesFragment extends BaseFragment implements Interactor {
         super.onResume();
 
         mMoviesViewModel.start();
+    }
+
+    private void setupToolbar() {
+        binding.toolbar.setTitle(R.string.app_name);
     }
 
     private void setupViewModels() {
@@ -77,5 +83,10 @@ public class MoviesFragment extends BaseFragment implements Interactor {
     @Override
     public void showMovieDetails(Movie movie) {
         Toast.makeText(getContext(), movie.toString(), Toast.LENGTH_SHORT).show();
+
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.container, MovieDetailsFragment.newInstance(movie.getId()));
+        ft.addToBackStack(null);
+        ft.commit();
     }
 }
